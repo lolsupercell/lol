916d8dda55df30c130415f9b4005b02a2b58c858
lolsupercell <lolsupercellol10@gmail.com>
2026-07-10 08:02:01 +0530

    Update CACHE_NAME to service-register-v16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "service-register-v15";
+const CACHE_NAME = "service-register-v16";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
